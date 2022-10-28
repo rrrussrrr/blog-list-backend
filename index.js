09c4b1ce1f4b90e22418a29ceaf505c88d05eca2
rrrussrrr <rruebner@gmail.com>
2022-10-28 14:55:04 -0400

    refactored to use config.js env variables

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const Blog = require('./models/blog')
+const config = require('./utils/config')
 
 
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl)
 
 app.use(cors())
@@ -33,7 +34,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
