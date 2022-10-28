3a4f180d18f7f3bd6d5bf36c08ace204d22edc82
rrrussrrr <rruebner@gmail.com>
2022-10-28 14:49:33 -0400

    utilizing blog.js exports

--- index.js
+++ index.js
@@ -5,15 +5,9 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
+const Blog = require('./models/blog')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
