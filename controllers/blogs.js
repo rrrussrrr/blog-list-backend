39a55889a0150e8cbb6a139a17c4cf3a78b7af0b
rrrussrrr <rruebner@gmail.com>
2022-11-06 18:50:20 -0500

    added delete and get by ID

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,23 +1,71 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
+// blogsRouter.get('/', (request, response) => {
+//     Blog
+//       .find({})
+//       .then(blogs => {
+//         response.json(blogs)
+//       })
+//   })
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+  const id = request.params.id.trim();
+
+  Blog.findById(id)
+    .then(blog => {
+      if (!blog) {
+        return response.status(404).end();
+      } else {
+        response.json(blog)
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(500).send({error: 'malformatted id'})
+    })
+})
+
+
   
-blogsRouter.post('/', (request, response) => {
-    console.log("req: ", request.body)
-    const blog = new Blog(request.body)
-    console.log(blog)
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+// blogsRouter.post('/', (request, response) => {
+//     console.log("req: ", request.body)
+//     const blog = new Blog(request.body)
+//     console.log(blog)
+//     blog
+//       .save()
+//       .then(result => {
+//         response.status(201).json(result)
+//       })
+//   })
+
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
   })
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog);
+  } catch(exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  Blog.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+})
 
   module.exports = blogsRouter
